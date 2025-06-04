75dcd0b65c31881d9b5d2d43717a67a49c02b19e
Tyron Landman <tyron.j.landman@gmail.com>
2025-06-04 20:49:05 +1200

    fixed some resizing issues, now when it gets smaller it will resize to make it look nicer

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,36 +1,38 @@
 import Header from "./Header";
 import Page from "./Page";
-import moneyShot from "../assets/centalImage.jpg";
 import AboutMe from "../content/AboutMe";
 import "./HomePage.css";
+import DeveloperFound from "../content/DeveloperFound";
 
 function HomePage() {
   return (
     <>
       <Page>
-        <Header />
-        <div className="content">
-          <div className="topContent">
-            <h2 className="title">Middle Title!</h2>
+        <div className="allContent">
+          <Header />
+          <div className="content">
+            <h3 className="title">Middle Title!</h3>
             <div className="innerContent">
-              <AboutMe />
-              <div className="somethingSection">
-                <img src={moneyShot} className="mainImg" />
-                <h6> something to do with the picture </h6>
-                <h5>Current Employment: Fullstack Developer</h5>
-                <div className="two-column-text">
-                  Currently working for Accenture on a product called BlueWorx.
-                  BlueWorx is a mobile application that
-                </div>
+              <div className="sideSection">
+                <AboutMe />
               </div>
-              <div className="stacksSection">TechStacks</div>
+              <div className="mainSection">
+                <DeveloperFound />
+              </div>
+              <div className="sideSection">
+                <AboutMe />
+              </div>
             </div>
-          </div>
-          <div className="bottomContent">
             <div className="innerContent">
-              <div className="linksSection">Something</div>
-              <div className="experienceSection">Experience</div>
-              <div className="factsSection">facts</div>
+              <div className="mainSection">
+                <DeveloperFound />
+              </div>
+              <div className="sideSection">
+                <AboutMe />
+              </div>
+              <div className="sideSection">
+                <AboutMe />
+              </div>
             </div>
           </div>
         </div>
@@ -40,19 +42,3 @@ function HomePage() {
 }
 
 export default HomePage;
-
-/* 
-display:flex;
-flex-direction: column ( default)
-  sets the <-----> to the main axis
-  sets the ^
-           |  as the cross axis
-justify-content: decides distance between content on main axis
-
-align items adjusts the cross axis alignment
-
-align-content, only works when flex-wrap: wrap is set
-align everything on the cross axis
-
-
-*/
--- src/content/AboutMe.tsx
+++ src/content/AboutMe.tsx
@@ -1,11 +1,11 @@
-import SideContent from "../components/SideContent";
+import Content from "../components/Content";
 
 function AboutMe() {
   const data: string =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque et libero dui. Fusce convallis ligula leo, at tempor dolor ultricies sed. Donec finibus gravida neque, et";
   return (
     <>
-      <SideContent data={data} />
+      <Content data={data} />
     </>
   );
 }
